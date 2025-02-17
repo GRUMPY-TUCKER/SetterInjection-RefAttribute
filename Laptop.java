96041f89abdee4c9f8086425add6013007241883
RUPAM SADHUKHAN <115567972+GRUMPY-TUCKER@users.noreply.github.com>
2025-02-17 15:54:20 +0530

    Add files via upload

---
+++ Laptop.java
@@ -0,0 +1,14 @@
+package org.maven;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+    public Laptop(){
+        System.out.println("Laptop Object Created!");
+    }
+    public void compile(){
+        System.out.println("Compiling");
+    }
+}
